a210705ece018f85ff5f6e4f47c6318c589c86ba
asimaranov <a.simaranov@gmail.com>
2024-07-11 17:59:51 +0300

    Updated lottery addresss

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -3,6 +3,6 @@ import { NETWORKS, NetworkIds } from './networks';
 export const LOTTERY_ADDRESS: {
   readonly [networkId: string]: string | 'not-deployed';
 } = {
-  [NetworkIds.MINA_DEVNET]: 'B62qkukYhz7mr9Rq6ucGPjGDpyxvKVQQrkxQSd2QpgHCe5gotY4JTEi',
+  [NetworkIds.MINA_DEVNET]: 'B62qnQELCoxrQp7fHhBSmEr3qMRDipZrxb6HPFpv3U5feadJjsfJPXn',
   [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
 };
