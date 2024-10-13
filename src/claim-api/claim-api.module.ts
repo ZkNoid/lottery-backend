5f79c56548183e8a7c85443aa8f6a215f2cad9ab
asimaranov <a.simaranov@gmail.com>
2024-10-14 03:42:13 +0700

    Fixed claim api module

--- src/claim-api/claim-api.module.ts
+++ src/claim-api/claim-api.module.ts
@@ -8,17 +8,27 @@ import {
 import { ClaimApiService } from './claim-api.service.js';
 import { ClaimApiController } from './claim-api.controller.js';
 import { StateModule } from '../state-service/state.module.js';
+import { ZknoidConfigModule } from '../config/config.module.js';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    ZknoidConfigModule,
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.MONGODB_URI,
+        dbName: process.env.MONGODB_DB,
+      }),
+      inject: [ConfigService],
+    }),
     MongooseModule.forFeature([
       {
         name: RoundsData.name,
         schema: RoundsDataSchema,
       },
     ]),
-    StateModule,
+    StateModule
   ],
   providers: [ClaimApiService],
   controllers: [ClaimApiController],
