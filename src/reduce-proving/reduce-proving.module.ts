5e61275d2fcd83b5fe1b7a4050c997d7be9e39b0
asimaranov <a.simaranov@gmail.com>
2024-12-25 08:18:58 +0800

    Fixed reduce prover

--- src/reduce-proving/reduce-proving.module.ts
+++ src/reduce-proving/reduce-proving.module.ts
@@ -15,7 +15,7 @@ import { ZknoidConfigModule } from '../config/config.module.js';
 import { ConfigService } from '@nestjs/config';
 import { SyncEventsModule } from '../workers/sync-events/sync-events.module.js';
 import { HealthController } from '../health-api/health-api.controller.js';
-import { RoundInfoUpdaterModule } from 'src/workers/round-infos-updater/round-infos-updater.module.js';
+import { RoundInfoUpdaterModule } from '../workers/round-infos-updater/round-infos-updater.module.js';
 
 
 @Module({
